1b6c594de5b8392f2239ec6270aa7d1c3e618e95
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-03-04 19:55:17 +0100

    Add files via upload

---
+++ python/shape_calculator.py
@@ -0,0 +1,52 @@
+class Rectangle:
+	
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+	
+	def set_width(self, width):
+		self.width = width
+	
+	def set_height(self, height):
+		self.height = height
+	
+	def get_area(self):
+		return self.width * self.height
+	
+	def get_perimeter(self):
+		return 2 * self.width + 2 * self.height
+	
+	def get_diagonal(self):
+		return (self.width ** 2 + self.height ** 2) ** .5
+	
+	def get_picture(self):
+		pass
+	
+	def get_amount_inside(self):
+		pass
+	
+	def __str__(self):
+		return f"Rectangle(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+	pass
+
+
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+#sq = Square(9)
+#print(sq.get_area())
+#sq.set_side(4)
+#print(sq.get_diagonal())
+#print(sq)
+#print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+#print(rect.get_amount_inside(sq))
