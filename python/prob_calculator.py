40b42f22feee04c502c34b66396f27cf82f18013
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-03-07 21:21:40 +0100

    Add files via upload

---
+++ python/prob_calculator.py
@@ -0,0 +1,19 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+class Hat:
+	
+	def draw(self, numberOfBalls):
+		pass
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+	pass
+
+
+#hat1 = Hat(yellow=3, blue=2, green=6)
+#hat2 = Hat(red=5, orange=4)
+#hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+
+#hat = Hat(black=6, red=4, green=3)
+#probability = experiment(hat=hat,expected_balls={"red":2,"green":1},num_balls_drawn=5,num_experiments=2000)
