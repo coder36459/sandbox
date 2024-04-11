579985b003d412b98fd69ce4ddda0539ed98ea56
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-04-11 21:50:16 +0200

    Add files via upload

---
+++ javascript/how-many-days-left.js
@@ -0,0 +1,8 @@
+"use strict";
+const date = new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
