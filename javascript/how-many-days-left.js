3802c96485c5ed63772254d19aec40b44e367de1
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-04-12 20:02:33 +0200

    Update how-many-days-left.js

--- javascript/how-many-days-left.js
+++ javascript/how-many-days-left.js
@@ -1,8 +1,17 @@
 "use strict";
 const date = new Date();
 console.log(date.getFullYear());
-console.log(date.getMonth());
+const month = date.getMonth() + 1;
+let s = "";
 console.log(date.getDay());
 console.log(date.getHours());
 console.log(date.getMinutes());
 console.log(date.getSeconds());
+console.log();
+if (month < 10) {
+	s = "0" + String(month);
+}
+else {
+	s = String(month);
+}
+console.log(s);
