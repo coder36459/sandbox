ef157b3dd17cf8f5465d3dccbcbc51160c23407c
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-04-20 09:38:05 +0200

    Update how-many-days-left.js

--- javascript/how-many-days-left.js
+++ javascript/how-many-days-left.js
@@ -6,15 +6,44 @@ const day = date.getDate();
 const hour = date.getHours();
 const minute = date.getMinutes();
 const second = date.getSeconds();
-let formatDT = [];
-function lessThanTen(n) {
-	let s = String(n);
-	s = "0" + s;
-	return s;
+function formatDateTime (y,m,d,h,i,s) {
+	function lessThanTen(n) {
+		let s = String(n);
+		s = "0" + s;
+		return s;
+	}
+	let dt = "";
+	dt += y + "-";
+	if (m < 10) {
+		dt += lessThanTen(m) + "-";
+	}
+	if (m >= 10) {
+		dt += m + "-";
+	}
+	if (d < 10) {
+		dt += lessThanTen(d) + " ";
+	}
+	if (d >= 10) {
+		dt += d + " ";
+	}
+	if (h < 10) {
+		dt += lessThanTen(h) + ":";
+	}
+	if (h >= 10) {
+		dt += h + ":";
+	}
+	if (i < 10) {
+		dt += lessThanTen(i) + ":";
+	}
+	if (i >= 10) {
+		dt += i + ":";
+	}
+	if (s < 10) {
+		dt += lessThanTen(s);
+	}
+	if (s >= 10) {
+		dt += s;
+	}
+	return dt;
 }
-if (month < 10) {
-	console.log(lessThanTen(month));
-}
-else {
-	console.log(String(month));
-}
+console.log(formatDateTime (year,month,day,hour,minute,second));
