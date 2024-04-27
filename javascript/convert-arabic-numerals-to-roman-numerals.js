ab40904fe0f63211e91386931a30f5ca9cfa04cf
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-04-27 11:23:51 +0200

    Update convert-arabic-numerals-to-roman-numerals.js

--- javascript/convert-arabic-numerals-to-roman-numerals.js
+++ javascript/convert-arabic-numerals-to-roman-numerals.js
@@ -1,6 +1,10 @@
 "use strict";
+const n = [1,5,10,50,100,500,1000];
+const l = ["I","V","X","L","C","D","M"];
 const arabicNumeral = 8;
 let romanNumeral = "";
+console.log(n.length);
+console.log(l.length);
 if ((arabicNumeral % 5) > 0) {
 	//console.log(Math.floor(arabicNumeral / 5));
 	romanNumeral += "V";
