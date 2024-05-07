b77c773dec63f6930ad6f78b42d4358a3dd0497b
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-05-07 20:45:13 +0200

    Update convert-any-letter-to-a-number-from-one-to-nine.js

--- javascript/convert-any-letter-to-a-number-from-one-to-nine.js
+++ javascript/convert-any-letter-to-a-number-from-one-to-nine.js
@@ -1,9 +1,19 @@
 "use strict";
 const anyString = "ABC";
 const letters = [];
-let i = 97;
+let i = 97, j = 0, x;
 while (i < 123) {
 	letters.push(String.fromCharCode(i).toUpperCase());
+	if (j + 1 > 9) {
+		j = 0;
+		letters.push(j += 1);
+	}
+	else {
+		letters.push(j += 1);
+	}
 	i += 1;
+}
+for (x in letters) {
+	console.log(letters[x]);
 }
 console.log(letters);
