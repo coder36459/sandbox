51b4cccae402830bd9e33d3d924a5b5b62c0d927
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-05-09 21:14:27 +0200

    Update convert-any-letter-to-a-number-from-one-to-nine.js

--- javascript/convert-any-letter-to-a-number-from-one-to-nine.js
+++ javascript/convert-any-letter-to-a-number-from-one-to-nine.js
@@ -1,24 +1,27 @@
 "use strict";
 const anyString = "ABC";
-const letters = [];
-let i = 97, j = 0, aS = [], number = 0, x, y;
-while (i < 123) {
-	letters.push(String.fromCharCode(i).toUpperCase());
-	if (j + 1 > 9) {
-		j = 0;
-		letters.push(j += 1);
+function converLetter(str) {
+	const letters = [];
+	let i = 97, j = 0, aS = [], number = 0, x, y;
+	while (i < 123) {
+		letters.push(String.fromCharCode(i).toUpperCase());
+		if (j + 1 > 9) {
+			j = 0;
+			letters.push(j += 1);
+		}
+		else {
+			letters.push(j += 1);
+		}
+		i += 1;
 	}
-	else {
-		letters.push(j += 1);
-	}
-	i += 1;
-}
-aS = anyString.split("");
-for (x in letters) {
-	for (y in aS) {
-		if (letters[x] == aS[y]) {
-			number += letters[parseInt(x) + 1];
+	aS = str.split("");
+	for (x in letters) {
+		for (y in aS) {
+			if (letters[x] == aS[y]) {
+				number += letters[parseInt(x) + 1];
+			}
 		}
 	}
+	return number;
 }
-console.log(number);
+console.log(converLetter(anyString));
